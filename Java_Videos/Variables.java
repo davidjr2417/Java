05cf62d2fa0e34b7230969135102a3b783773a73
davidjr2417 <david@hiddengeniusproject.org>
2021-08-20 12:41:56 -0700

    Added Comment

--- Java_Videos/Variables.java
+++ Java_Videos/Variables.java
@@ -2,7 +2,8 @@ package Java_Videos;
 
 public class Variables {
     public static void main(String[] args){
-       int myInt = 10;
+        /*Comment*/
+        int myInt = 10;
        double myDouble = 12.12;
        System.out.println(myInt);
        System.out.println(myDouble);
