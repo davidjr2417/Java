50f2d8b1077331c6feb1b1ef203fe2711b055af6
davidjr2417 <david@hiddengeniusproject.org>
2021-08-20 12:27:30 -0700

    Variables and Data Types

---
+++ Java_Videos/Variables.java
@@ -0,0 +1,10 @@
+package Java_Videos;
+
+public class Variables {
+    public static void main(String[] args){
+       int myInt = 10;
+       double myDouble = 12.12;
+       System.out.println(myInt);
+       System.out.println(myDouble);
+     }
+}
