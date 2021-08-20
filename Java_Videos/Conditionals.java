2a327e302e9ec39fc3bf9f400e10614d2a43c44c
davidjr2417 <david@hiddengeniusproject.org>
2021-08-20 13:16:01 -0700

    Added Conditions.java

---
+++ Java_Videos/Conditionals.java
@@ -0,0 +1,16 @@
+package Java_Videos;
+
+public class Conditionals {
+    public static void main(String[] args){
+        String x ="Good";
+        if(x=="Good"){
+            System.out.println("That Is Great!");
+        }else {
+            System.out.println("Hope You Have A Great Day!");
+        }
+           
+     }
+ 
+ 
+  
+}
