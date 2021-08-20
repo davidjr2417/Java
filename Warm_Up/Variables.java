334119914de6caefac5f5580eed6ca5b9aa5d2a9
davidjr2417 <david@hiddengeniusproject.org>
2021-08-20 13:17:50 -0700

    Updated Varaibles

--- Warm_Up/Variables.java
+++ Warm_Up/Variables.java
@@ -4,5 +4,6 @@ public class Variables {
     public static void main(String[] args){
         String x ="Test";
          System.out.println("Test");
+         System.out.println("Test");
      }
 }
