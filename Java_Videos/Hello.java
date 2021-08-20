d7c66174776be60905376a27335025b24b5f1d99
davidjr2417 <david@hiddengeniusproject.org>
2021-08-20 12:13:13 -0700

    Updated Folder Structure & Hello World Program

--- Java_Videos/Hello.java
+++ Java_Videos/Hello.java
@@ -1,10 +1,10 @@
+package Java_Videos;
 /*
 * Hellow World Program
 */
 
 public class Hello{
 
-`
     public static void main(String[] args){
        String x ="Test";
         System.out.println("Test");
